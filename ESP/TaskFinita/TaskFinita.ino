f078ea1f059005f901760c43662988fef026ab1b
НТО ЦСС <dss@dt.net>
2024-04-04 14:48:22 +0300

    Some changes

--- ESP/TaskFinita/TaskFinita.ino
+++ ESP/TaskFinita/TaskFinita.ino
@@ -5,15 +5,14 @@
 // #define MAX_LINE_LENGTH (64)
 #define FORMAT_LITTLEFS_IF_FAILED true
 
-const char ssid[] = "Redmi Note 10S";
-const char pass[] = "11111111";
-const char *ip = "192.168.123.176";
+const char ssid[] = "ternary_q";
+const char pass[] = "55555555";
+const char *ip = "192.168.43.130";
 int port = 7001;
 
 const int buff_size = 1000;
 int frames = 10;
 
-int boba[30][1000];
 
 int ind=0;
 
@@ -27,13 +26,12 @@ int k =0;
 
 // TaskHandle_t task_send, task_rot;
 hw_timer_t *My_timer = NULL;
+TaskHandle_t handler_rec;
 
-int packet1_num = 0;
+int packet1_num = 1;
 
-
-
-WiFiServer server(80);
-
+WiFiClient client;
+WiFiServer server(7020);
 
 // void appendFile(fs::FS &fs, const char * path, const char * message){
 //     Serial.printf("Appending to file: %s\r\n", path);
@@ -83,35 +81,52 @@ WiFiServer server(80);
 //     file.close();
 // }
 
+void listen_for_rec(void *params) {
+  while (true) {
+    WiFiClient cl = server.available();
+    if (cl) {
+      Serial.println("connection success");
+      while (cl.connected()) {
+        if (cl.available()) {
+          String cmd = cl.readString();
+          Serial.printf("CMD: %s\n", cmd.c_str());
+          if (cmd == "REC") {
+            Serial.println("Must start rec");
+          }
+        }
+        delay(10);
+      }
+    }
+    delay(150);
+  }
+}
+
 void ss(void*params){
-  WiFiClient client;
-  client.connect(ip, port);
+
   if (client.connected()){
+    delay(75);
     std::string packet = "WAV ";
+    // client.printf(packet.c_str());
+    packet +="0 30 ";
     packet += std::to_string(packet1_num);
-    packet += " ";
-    // packet += std::to_string(j);
-    packet += std::to_string(frames);
-    packet += " ";
-    packet += std::to_string(buff_size);
-    packet += " ";
-    packet += std::to_string(freq);
+    packet += " 1000S";
     client.printf(packet.c_str());
+    delay(75);
+    // Serial.println(packet.c_str());
     packet = "";
-    for (int j = 0; j < 10; j++) {
-      for (int i = 100*j; i < 100*(j+1) ; i++){
+    for (int j = 0; j < 1000; j++) {
         // Serial.println(buffer[i]);
         // client.printf("%d ", buffer[i]);
         if (choice)
-          packet += std::to_string(buffer[i]);
+          packet += std::to_string(buffer[j]);
         else
-          packet += std::to_string(buffera[i]);
-        if (i != buff_size - 1) {
-          packet += " ";
-        }
-        delay(1);
-      }
+          packet += std::to_string(buffera[j]);
+
+        packet += " ";
+
+    }
       client.printf(packet.c_str());
+      
       // if (k==0){
       //   k+=1;
       //   writeFile(LittleFS, "/data.txt", packet.c_str());
@@ -119,7 +134,6 @@ void ss(void*params){
       //   appendFile(LittleFS, "/data.txt", packet.c_str());
       // }
       packet = "";
-    }
     // Serial.println(packet.c_str());
 
     // String response = client.readString();
@@ -127,12 +141,17 @@ void ss(void*params){
     //   return;
     // }
     // Serial.printf("Server response: %s\n", response.c_str());
-    client.stop();
-    Serial.println("Disconnected");
+
     packet1_num++;
     Serial.println(packet1_num);
-  }else
-    Serial.println("Not able to connect");
+    }else
+      Serial.print("#");
+      // client.connect(ip, port);
+    if (packet1_num==31){
+      client.stop();
+      Serial.println("Disconnected");
+    }
+    
   vTaskDelete(NULL);
 }
 
@@ -142,23 +161,24 @@ void ss(void*params){
 void IRAM_ATTR onTimer(){
   // message_t message;
   if (!choice){
-    buffer[pointer] = analogRead(34);
-    buffer[pointer] &= 0x0FFF;
+    buffer[pointer] = analogRead(35);
+    // buffer[pointer] &= 0x0FFF;
   }
   else{
-    buffera[pointer] = analogRead(34);
-    buffera[pointer] &= 0x0FFF;
+    buffera[pointer] = analogRead(35);
+    // buffera[pointer] &= 0x0FFF;
   }
   ind++;
   pointer++;
-    if(pointer == buff_size){
+    if (pointer == buff_size) {
       pointer = 0;
       choice = !choice;
       xTaskCreatePinnedToCore(ss, "send", 10000, NULL, 1, NULL, 1);
       // int ret = xQueueSend(QueueHandle, (void*) &message, 0);
 
     }
-  if (ind==30000){
+  if (ind>30000){
+    
     timerDetachInterrupt(My_timer);
   }
   // if (pointer == 7999){
@@ -200,16 +220,22 @@ void setup() {
     Serial.println("[WIFI_SUCCESS]");
     Serial.printf("IP: %s\n", WiFi.localIP().toString().c_str());
   }
+  
+  server.begin();
+  client.connect(ip, port);
+  while (!client.connected()){
+    Serial.print(".");
+    delay(100);
+    // client.connect(ip, port);
+  }
 
   // if(!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
   //       Serial.println("LittleFS Mount Failed");
   //   }
 
   xTaskCreatePinnedToCore(setupTimer, "time", 10000, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(listen_for_rec, "listen_rec", 10000, NULL, 1, &handler_rec, 1);
   // xTaskCreatePinnedToCore(recieve, "recieve", 10000, NULL, 1, NULL, 0);
-
-
-
 }
 void loop() {
  delay(100);
